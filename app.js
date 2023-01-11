dc50236e884605b6410b74ee7eba3369412d5ee3
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2023-01-11 12:02:37 +0100

    Implemented light/dark mode + code clean

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-/*let themeToggle = document.getElementById("themeToggle");
-console.log(themeToggle);
-let bodyTheme = document.getElementsByTagName("body");
-console.log(bodyTheme);*/
-
-/*themeToggle.addEventListener('click', () => {
-    if () {
-        
-    }
-})*/
+// Hamburger menu
 
 let hambMenu = document.getElementById("menuHamburger");
-console.log(hambMenu);
 let menuHamburger = document.querySelector(".hamMenu");
-console.log(menuHamburger);
 hambMenu.addEventListener("click", () => {
   hambMenu.classList.toggle("active");
   menuHamburger.classList.toggle("active");
 });
+
+// Dark mode
+
+let themeToggle = document.getElementById("themeToggle");
+let bodyTheme = document.getElementsByTagName("body");
+themeToggle.addEventListener("click", () => {
+  // bodyTheme[0] represents the body tag itself, not the object body as a whole
+  bodyTheme[0].classList.toggle("darkMode");
+  bodyTheme[0].classList.toggle("lightMode");
+});
+
+// Cookies to save preferences over light/dark mode

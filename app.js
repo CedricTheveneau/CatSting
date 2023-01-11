bb0c5b8ba38b8633ae732d9d839b9dc062939551
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2023-01-11 12:06:24 +0100

    Added the switch effect on the theme toggle

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ hambMenu.addEventListener("click", () => {
 // Dark mode
 
 let themeToggle = document.getElementById("themeToggle");
+let sun = document.getElementById("sun");
+let moon = document.getElementById("moon");
 let bodyTheme = document.getElementsByTagName("body");
 themeToggle.addEventListener("click", () => {
   // bodyTheme[0] represents the body tag itself, not the object body as a whole
   bodyTheme[0].classList.toggle("darkMode");
   bodyTheme[0].classList.toggle("lightMode");
+  sun.classList.toggle("hide");
+  moon.classList.toggle("hide");
 });
 
 // Cookies to save preferences over light/dark mode

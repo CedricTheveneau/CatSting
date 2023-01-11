d219153635a74a474b4b87cf4ca04b9f29ccf07d
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2023-01-11 10:03:52 +0100

    Created the project's root and file tree

---
+++ app.js
@@ -0,0 +1,14 @@
+let themeToggle = document.getElementById("themeToggle");
+console.log(themeToggle);
+let bodyTheme = document.getElementsByTagName("body");
+console.log(bodyTheme);
+
+/*themeToggle.addEventListener('click', () => {
+    if () {
+        
+    }
+})*/
+
+/*let hambMenu = document.getElementById("menuHamburger");
+hambMenu.addEventListener('click' () => {
+})*/

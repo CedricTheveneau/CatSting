4e2170258b516cb16960de42b6b01741b198109a
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2023-01-12 16:10:58 +0100

    Random anecdotes done

---
+++ public/pages/anecdote/anecdotes.js
@@ -0,0 +1,22 @@
+// Fetch anecdotes
+let anecdotes = () => {
+  let fetchAnecdotes = () => {
+    let anecdoteBox = document.getElementById("anecdote");
+    let anecdote;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://catfact.ninja/fact", false);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        let response = JSON.parse(xhr.responseText);
+        anecdote = response.fact;
+      }
+    };
+    xhr.send();
+    anecdoteBox.innerHTML = anecdote;
+  };
+  let showMeTrivia = document.getElementById("showMeTrivia");
+  showMeTrivia.addEventListener("click", () => {
+    fetchAnecdotes();
+  });
+};
+anecdotes();
